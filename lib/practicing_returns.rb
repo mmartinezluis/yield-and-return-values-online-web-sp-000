a689de1886817b02dbba8c80d1f0090fb61c333a
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 05:01:55 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,9 +4,10 @@ def hello(array)
   i = 0
   while i < array.length
     yield(array[i])
+    binding.pry
     i += 1
   end
 end
 
-binding.pry
+
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
